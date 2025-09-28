ace7f37a24036c2b9e79e13f1bf698570b2171e8
Luis Sanchez <luisanchezdh@gmail.com>
2025-09-28 19:56:20 -0400

    added valid/invalid pseudo-classes and CSS

--- script.js
+++ script.js
@@ -8,15 +8,49 @@ const postalCodeInput = form.elements.postalCode
 const passwordInput = form.elements.password
 const confirmInput = form.elements.confirmPassword
 
-console.log(form)
-console.log(emailInput,countryInput,postalCodeInput,passwordInput,confirmInput)
-
+console.log(form,emailInput)
 
 const emailError = emailInput.nextElementSibling
 const countryError = countryInput.nextElementSibling
 const postalCodeError=postalCodeInput.nextElementSibling
 const passwordError = passwordInput.nextElementSibling
 const confirmError = confirmInput.nextElementSibling
-console.log(emailError, countryError, postalCodeError, passwordError, confirmError)
+
+function validateEmail(email){
+    if(email===""){
+        return false
+    }
+
+    if(!email.includes("@")){
+        return false
+    }
+
+    const parts = email.split("@");
+    
+    const domain = parts[1]
+    if(!domain||!domain.includes(".")){
+        return false
+    }
+    const domainParts = domain.split(".")
+    const lastPart =domainParts[domainParts.length -1]
+    if(lastPart.length<2){
+        return false
+    }
+    return true;
+}
+
+emailInput.addEventListener("blur",(e)=>{
+    const userEmail = e.target.value
+    if(validateEmail(userEmail)===true){
+        emailError.textContent="";
+        emailInput.classList.add("valid")
+        emailInput.classList.remove('invalid')
+        
+    } else{
+        emailError.textContent="Please enter a valid email address"
+        emailInput.classList.remove("valid")
+        emailInput.classList.add("invalid")
 
 
+}
+})
